805b4fa28aaed83d0d0999a8d87616f69e0dd366
Isyraf <isyrafsan97@gmail.com>
2026-04-02 20:42:03 +0800

    add sooner toaster

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import { DashboardSidebar } from '@/components/shared/DashboardSidebar'
 import { DashboardTopbar } from '@/components/shared/DashboardTopbar'
 import { AuthInitializer } from '@/components/shared/AuthInitializer'
 import { AuthGuard } from '@/components/shared/AuthGuard'
+import { Toaster } from 'sonner'
 
 export default function DashboardLayout({
   children,
@@ -17,6 +18,7 @@ export default function DashboardLayout({
         <DashboardTopbar />
         <main className="flex-1 overflow-y-auto p-6">{children}</main>
       </div>
+      <Toaster richColors position="top-right" />
     </div>
   )
 }
